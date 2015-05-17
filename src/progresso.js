1aaaf6a5bb2ce2813e1be4c0bc576e9031d3c229
Sean Michael <seankmichael@gmail.com>
2015-05-16 21:20:37 -0600

    rando

--- src/progresso.js
+++ src/progresso.js
@@ -7,15 +7,17 @@
 		// --------------------------------------------------
 		var pluginName = "progresso",
 			defaults = {
-				propertyName: "value"
+				output: "integer"
 			};
 
 		// --------------------------------------------------
 		// CONSTRUCTOR
 		// --------------------------------------------------
-		function Progresso ( element, options ) {
+		function Progresso ( options ) {
 			this.element = element;
 			this.settings = $.extend( {}, defaults, options );
+			this.progress = 0;
+			this.assets = [];
 			this._defaults = defaults;
 			this._name = pluginName;
 			this.init();
@@ -26,10 +28,31 @@
 		// --------------------------------------------------	
 		$.extend(Progresso.prototype, {
 			init: function () {
-				this.yourOtherFunction(this.element, this.settings);
+				getAssets();
 			},
-			yourOtherFunction: function () {
-				// some logic
+			getAssets: function() {
+				$element.find('*:not(script)').each(function() {
+					var url = "";
+					if ($(this).css('background-image').indexOf('none') == -1 && $(this).css('background-image').indexOf('linear-gradient') == -1) {
+
+						url = $(this).css('background-image');
+						if(url.indexOf('url') != -1) {
+							var temp = url.match(/url\((.*?)\)/);
+							url = temp[1].replace(/\"/g, '');
+						}
+					
+					} else 
+					if($(this).get(0).nodeName.toLowerCase() == 'img' && typeof($(this).attr('src')) != 'undefined') {
+						url = $(this).attr('src');
+					}
+					
+					if (url.length > 0) {
+						items.push(url);
+					}
+
+				});
+
+				return items;
 			}
 		});
 
